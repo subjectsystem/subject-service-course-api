9f7b711064aac0db65cfd5dc8007a524a374395b
lanxiao wei <weilanxiao@163.com>
2019-08-07 11:24:32 +0800

    初始化项目。。。

---
+++ src/main/java/com/miyako/subject/service/course/api/TbCourseService.java
@@ -0,0 +1,9 @@
+package com.miyako.subject.service.course.api;
+
+import com.miyako.subject.commons.domain.TbCourse;
+
+import java.util.List;
+
+public interface TbCourseService{
+    public List<TbCourse> selectAll();
+}
