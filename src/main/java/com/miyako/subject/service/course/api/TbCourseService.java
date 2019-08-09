f3cef502ed94808c4ecc3d3e3bf4db12c3a4ad18
lanxiao wei <weilanxiao@163.com>
2019-08-09 17:04:16 +0800

    课程列表分页简单实现。。.

--- src/main/java/com/miyako/subject/service/course/api/TbCourseService.java
+++ src/main/java/com/miyako/subject/service/course/api/TbCourseService.java
@@ -1,9 +1,22 @@
 package com.miyako.subject.service.course.api;
 
+import com.github.pagehelper.PageInfo;
 import com.miyako.subject.commons.domain.TbCourse;
 
 import java.util.List;
 
 public interface TbCourseService{
+
     public List<TbCourse> selectAll();
+
+    public TbCourse selectById(Integer id);
+
+    public TbCourse selectOne(TbCourse tbCourse);
+
+    public Integer insert(TbCourse tbCourse);
+
+    public PageInfo<TbCourse> page(int pageNum, int pageSize);
+
+    public int update(TbCourse tbCourse);
+
 }
